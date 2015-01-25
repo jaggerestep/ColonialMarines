jaggerestep/ColonialMarines

commit dc29565d0c9fa7c9da6f344f94529790bca9ca18

Addition of Rebel Boxes
For easier Marine Vs Rebel rounds

code/modules/marines/closets.dm
@@ -354,3 +354,21 @@
 		new /obj/item/device/radio/headset/mdeltal(src)
 		return
 
+
+
+/obj/structure/closet/crate/plastic/marine/REBEL
+	name = "REBEL Supply Crate"
+	desc = "A crate with additional REBEL ALLIANCE Supplies"
+	New()
+		..()
+		for(var/c, c<5, c++)
+			new /obj/item/clothing/under/color/grey(src)
+			new /obj/item/clothing/head/helmet/swat(src)(src)
+			new /obj/item/clothing/suit/armor/vest(src)
+			new /obj/item/clothing/shoes/marine(src)
+			new /obj/item/weapon/storage/belt/marine(src)
+			new /obj/item/weapon/storage/belt/utility/full(src)
+			new /obj/item/device/multitool(src)
+			new /obj/item/weapon/storage/backpack(src)
+		return
+

commit 0b68551d4185e5596745efcb2b9777404f95e030

Fixed a warning caused by dc29565d0c9fa7c9da6f344f94529790bca9ca18

code/modules/marines/closets.dm
@@ -363,7 +363,7 @@
 		..()
 		for(var/c, c<5, c++)
 			new /obj/item/clothing/under/color/grey(src)
-			new /obj/item/clothing/head/helmet/swat(src)(src)
+			new /obj/item/clothing/head/helmet/swat(src)
 			new /obj/item/clothing/suit/armor/vest(src)
 			new /obj/item/clothing/shoes/marine(src)
 			new /obj/item/weapon/storage/belt/marine(src)